6708b4a1fbaf8b6b85290da5a39d6b4ce2be3736
Susanne Moog <look@susi.dev>
2020-03-09 18:31:58 +0100

    [TASK] Add functionality

--- Classes/Hooks/PageRendererPreProcess.php
+++ Classes/Hooks/PageRendererPreProcess.php
@@ -23,10 +23,92 @@ class PageRendererPreProcess
 
     public function addLibrary(&$params, &$pagerenderer): void
     {
+        $config = $this->getTypoScriptConfiguration();
+        if ($config === null) {
+            return;
+        }
+        if (!$this->isValidId($config)) {
+            throw new \InvalidArgumentException('Usercentrics ID not configured, please set plugin.tx_usercentrics.id in your TypoScript configuration', 1583774571);
+        }
+        $this->addUserCentricsScript($config['id']);
+        $this->addConfiguredJsFiles($config['jsFiles.']);
+    }
+
+    /**
+     * @param $jsFiles
+     */
+    protected function addConfiguredJsFiles($jsFiles): void
+    {
+        foreach ($jsFiles ?? [] as $jsFile) {
+            if(!$this->isValidFile($jsFile)) {
+                throw new \InvalidArgumentException('No valid file given, please check TypoScript configuration.', 1583774682);
+            }
+            if (!$this->isValidIdentifier($jsFile)) {
+                throw new \InvalidArgumentException('No valid identifier given for file, please check TypoScript configuration.', 1583774683);
+            }
+            $identifier = $jsFile['identifier'];
+            $attributes = $this->getAttributesForUsercentrics($jsFile['attributes.'] ?? [], $identifier);
+            $options = $this->convertPriorityToBoolean($jsFile['options.'] ?? []);
+            $this->assetCollector->addJavaScript($identifier, $jsFile['file'], $attributes, $options);
+        }
+    }
+
+    /**
+     * @param $id
+     */
+    protected function addUserCentricsScript($id): void
+    {
         $this->assetCollector->addJavaScript('usercentrics', 'https://app.usercentrics.eu/latest/main.js', [
             'type' => 'application/javascript',
-            'id' => 'getMeFromTypoScript'
+            'id' => $id
         ]);
-        // do something with the JS Files stuff from TypoScript
+    }
+
+    /**
+     * @param array $options
+     * @return array
+     */
+    protected function convertPriorityToBoolean(array $options): array
+    {
+        if (!empty($options['priority'])) {
+            // make it a real boolean
+            $options['priority'] = true;
+        }
+        return $options;
+    }
+
+
+    protected function getAttributesForUsercentrics(array $attributes, string $identifier): array
+    {
+        $attributes['type'] = 'text/plain';
+        $attributes['data-usercentrics'] = $identifier;
+        return $attributes;
+    }
+
+    protected function getTypoScriptConfiguration(): ?array
+    {
+        /** @var \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController $tsfe */
+        $tsfe = $GLOBALS['TSFE'];
+        $ts = $tsfe->tmpl->setup;
+        return $ts['plugin.']['tx_usercentrics.'] ?? null;
+    }
+
+    protected function isValidFile($jsFile): bool
+    {
+        return isset($jsFile['file']) && is_string($jsFile['file']);
+    }
+
+    /**
+     * @param array $config
+     * @return bool
+     */
+    protected function isValidId(array $config): bool
+    {
+        return isset($config['id']) && is_string($config['id']);
+    }
+
+    protected function isValidIdentifier($jsFile): bool
+    {
+        return isset($jsFile['identifier']) && is_string($jsFile['identifier']);
     }
 }
