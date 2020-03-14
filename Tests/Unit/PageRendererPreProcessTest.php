5de41aa66ffd346a6ffebace7d42e5a87771045c
Susanne Moog <look@susi.dev>
2020-03-14 08:24:17 +0100

    [TASK] Fix test

--- Tests/Unit/PageRendererPreProcessTest.php
+++ Tests/Unit/PageRendererPreProcessTest.php
@@ -22,7 +22,7 @@ class PageRendererPreProcessTest extends UnitTestCase
     protected $templateService;
     protected $assetCollector;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
         $tsfeProphecy = $this->prophesize(TypoScriptFrontendController::class);
